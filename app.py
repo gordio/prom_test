7374dd17ea4aaff3f0c224b50a80422495deec7f
Oleg Gordienko <admin@gordio.pp.ua>
2014-05-04 13:44:57 +0300

    project: Add views

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, g, render_template, flash, redirect, session, request,\
-    url_for, abort
+from flask import Flask, g, render_template, flash, redirect, session, request, url_for, abort
 
 
 app = Flask(__name__, static_url_path='')
 app.config.from_pyfile('config.py')
+
+
+from models import db, Book, Author
+from forms import AuthorForm, BookForm
 
 
 # ERRORS
@@ -20,4 +23,45 @@ if not app.config['DEBUG']:
 # VIEWS
 @app.route('/')
 def index():
+    form = BookForm()
     return render_template('index.html', **locals())
+
+
+# Books
+@app.route('/book/view/<int:book_id>', methods=['GET','POST'])
+def book_view(book_id):
+    book = Book.query.get_or_404(book_id)
+    return render_template('books/view.html', **locals())
+
+
+@app.route('/book/add/', methods=['GET', 'POST'])
+def book_add():
+    form = BookForm(request.form)
+    form.authors.choices = [(a.id, a.title) for a in Author.query.all()]
+
+    if request.method == "POST" and form.validate():
+        # Add book
+        pass
+        # return redirect(url_for('book_view', book_id=book.id))
+
+    return render_template('books/add.html', **locals())
+
+
+# Authors
+@app.route('/author/view/<int:author_id>', methods=['GET'])
+def author_view(author_id):
+    author = Author.query.get_or_404(author_id)
+    return render_template('authors/view.html', **locals())
+
+
+@app.route('/author/add/', methods=['GET', 'POST'])
+def author_add():
+    form = AuthorForm(request.form)
+
+    if request.method == "POST" and form.validate():
+        new_author = Author(title=form.data.get('title'))
+        db.session.add(new_author)
+        db.session.commit()
+        return redirect(url_for('author_view', author_id=new_author.id))
+
+    return render_template('authors/add.html', **locals())
--- models.py
+++ models.py
@@ -16,22 +16,22 @@ class Book(db.Model):
     __tablename__ = "books"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(300), unique=True)
+    title = db.Column(db.String(300), unique=True)
     authors = db.relationship(
         'Author',
         secondary=books_authors,
         backref=db.backref('books', lazy='dynamic')
     )
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, title):
+        self.title = title
 
 
 class Author(db.Model):
     __tablename__ = "authors"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), unique=True)
+    title = db.Column(db.String(200), unique=True)
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, title):
+        self.title = title
