7acf4beb1fe36ddbfce9f6956e3bf3f9b210e075
Oleg Gordienko <admin@gordio.pp.ua>
2014-05-05 22:33:38 +0300

    forms: (fix) Use "sqlalchemy field"

--- forms.py
+++ forms.py
@@ -1,17 +1,23 @@
 from wtforms import Form, TextField, SelectMultipleField, validators
+from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField
+from models import Author
+
 
 class BookForm(Form):
     title = TextField('Title', [
         validators.Required(),
         validators.Length(min=4, max=200)
     ])
-    authors = SelectMultipleField(
-        'Authors',
-        [
-            validators.Required(),
-        ],
-        choices=[('0', 'Empty')]
-    )
+    authors = QuerySelectMultipleField('Authors', [
+        validators.Required(),
+    ], query_factory=lambda:Author.query.all())
+    # authors = SelectMultipleField(
+    #     'Authors',
+    #     [
+    #         validators.Required(),
+    #     ],
+    #     choices=[('0', 'Empty')]
+    # )
 
 
 class AuthorForm(Form):
